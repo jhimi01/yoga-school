051d7c170a2fa1f47d8dc4fe64f8c57c8838d19e
Jhimi Talukder <talukderjhimi@gmail.com>
2023-06-07 00:00:34 +0600

    firebaase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCMjPcYT9FIhbl3HUoeKeoKgqlhar-mKE",
+  authDomain: "yoga-school-7a5bc.firebaseapp.com",
+  projectId: "yoga-school-7a5bc",
+  storageBucket: "yoga-school-7a5bc.appspot.com",
+  messagingSenderId: "1072754004337",
+  appId: "1:1072754004337:web:a9707245eb369802e6e6d0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
