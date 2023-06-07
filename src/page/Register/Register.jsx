f688ab9a2a5d4bd843a4c28ab829bc319eb050c0
Jhimi Talukder <talukderjhimi@gmail.com>
2023-06-07 13:23:25 +0600

    confirm

--- src/page/Register/Register.jsx
+++ src/page/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
@@ -7,94 +7,105 @@ import { AiFillEye } from 'react-icons/ai';
 import { AiFillEyeInvisible } from 'react-icons/ai';
 
 const Register = () => {
-  const [show, setShow] = useState(false)
-    const { register, formState: { errors }, handleSubmit, control, reset} = useForm()
-    const { signupEmail, loginWithGoogle, updateUserProfile } = useContext(AuthContext)
-   
-    const onSubmit=(data)=>{
-      console.log(data)
-        reset()
-    }
+  const [show, setShow] = useState(false);
+  const [passwordMatch, setPasswordMatch] = useState(false); // Updated initial state to false
+  const { register, formState: { errors }, handleSubmit, control, reset, watch } = useForm();
+
+  const password = watch('password');
+
+  const { signupEmail, loginWithGoogle, updateUserProfile } = useContext(AuthContext);
+
+  // useEffect(() => {
+  //   setPasswordMatch(password === watch('confirm-password'));
+  // }, [password, watch]);
   
-    return (
-        <div className='md:flex items-center justify-between'>
-            <div className='w-full h-[100vh]'>
-                <img className='w-full h-full object-cover' src="https://i.ibb.co/h9mM4CV/pexels-hatice-baran-14252564.jpg" alt="" />
-            </div>
+  const onSubmit = (data) => {
+    if (data.password == data.confirm-password) {
+      console.log('sdfds')
+      setPasswordMatch(true)
+    }
+    console.log(data);
+    reset();
+  };
+  return (
+    <div className='md:flex items-center justify-between'>
+      <div className='w-full h-[100vh]'>
+        <img className='w-full h-full object-cover' src="https://i.ibb.co/h9mM4CV/pexels-hatice-baran-14252564.jpg" alt="" />
+      </div>
 
-            <div className='w-full mt-20'>
-            <h1 className='text-5xl font-semibold text-center'>Please Register</h1>
-            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Name</span>
-              </label>
-              <input type="text" {...register("name", { required: true })} placeholder="name" name="name" className="input input-bordered rounded-none" />
-              {errors.name && <span className="text-red-500 text-sm">This field is required</span>}
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input {...register("email", { required: true })} type="text" placeholder="email" className="input input-bordered rounded-none" />
-              {errors.email && <span className="text-red-500 text-sm">This field is required</span>}
-            </div>
-            <div className="form-control">
-  <label className="label">
-    <span className="label-text">Photo URL</span>
-  </label>
-  <input {...register("photo", { required: true })} type="url" placeholder="photo URL" className="input input-bordered rounded-none" />
-  {errors.photo && <span className="text-red-500 text-sm">This field is required</span>}
-</div>
+      <div className='w-full mt-20'>
+        <h1 className='text-5xl font-semibold text-center'>Please Register</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input type="text" {...register("name", { required: true })} placeholder="name" name="name" className="input input-bordered rounded-none" />
+            {errors.name && <span className="text-red-500 text-sm">This field is required</span>}
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input {...register("email", { required: true })} type="text" placeholder="email" className="input input-bordered rounded-none" />
+            {errors.email && <span className="text-red-500 text-sm">This field is required</span>}
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <input {...register("photo", { required: true })} type="url" placeholder="photo URL" className="input input-bordered rounded-none" />
+            {errors.photo && <span className="text-red-500 text-sm">This field is required</span>}
+          </div>
 
-            <div className="form-control relative">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input type={show? "text" : "password"} {...register("password", { 
-                required: true,
-                minLength: 7,
-                maxLength: 20,
-                 })} placeholder="password" className="input input-bordered rounded-none" />
-                 <p className='absolute right-3 top-[52px]' onClick={()=>{setShow(!show)}}>{show? <AiFillEyeInvisible className='text-xl'/> : <AiFillEye className='text-xl'/> }</p>
+          <div className="form-control relative">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input type={show ? "text" : "password"} {...register("password", {
+              required: true,
+              minLength: 7,
+              maxLength: 20,
+            })} placeholder="password" className="input input-bordered rounded-none" />
+            <p className='absolute right-3 top-[52px]' onClick={() => { setShow(!show) }}>{show ? <AiFillEyeInvisible className='text-xl' /> : <AiFillEye className='text-xl' />}</p>
 
-              {errors.password?.type === 'minLength' && <span className="text-red-500 text-sm">Password must be at least 7 characters</span>}
-              {errors.password?.type === 'maxLength' && <span className="text-red-500 text-sm">Password must be less than 20 characters</span>}
-              {errors.password?.type === 'required' && <span className="text-red-500 text-sm">This field is required</span>}
-            </div>
+            {errors.password?.type === 'minLength' && <span className="text-red-500 text-sm">Password must be at least 7 characters</span>}
+            {errors.password?.type === 'maxLength' && <span className="text-red-500 text-sm">Password must be less than 20 characters</span>}
+            {errors.password?.type === 'required' && <span className="text-red-500 text-sm">This field is required</span>}
+          </div>
 
-            {/* confirm password */}
-            <div className="form-control relative">
-              <label className="label">
-                <span className="label-text">Confirm Password</span>
-              </label>
-              <input type={show? "text" : "password"} {...register("confirm-password", { 
-                required: true,
-                minLength: 7,
-                maxLength: 20,
-                 })} placeholder="condirm password" className="input input-bordered rounded-none" />
-                 <p className='absolute right-3 top-[52px]' onClick={()=>{setShow(!show)}}>{show? <AiFillEyeInvisible className='text-xl'/> : <AiFillEye className='text-xl'/> }</p>
-{/* 
-             {errors.password?.type === 'minLength' && <span className="text-red-500 text-sm">Password must be at least 7 characters</span>}
-              {errors.password?.type === 'maxLength' && <span className="text-red-500 text-sm">Password must be less than 20 characters</span>}
-              {errors.password?.type === 'required' && <span className="text-red-500 text-sm">This field is required</span>}  */}
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <input className="btn bg-base-300 rounded-none" type="submit" value='signup' />
-              
-            </div>
-            <Link to='/login'><p className='text-sm text-blue-700 underline'>Already have an account?Login.</p></Link>
+          {/* confirm password */}
+          <div className="form-control relative">
+            <label className="label">
+              <span className="label-text">Confirm Password</span>
+            </label>
+            <input type={show ? "text" : "password"} {...register("confirm-password", {
+              required: true,
+              minLength: 7,
+              maxLength: 20,
+            })} placeholder="confirm password" className="input input-bordered rounded-none" />
+            <p className='absolute right-3 top-[52px]' onClick={() => { setShow(!show) }}>{show ? <AiFillEyeInvisible className='text-xl' /> : <AiFillEye className='text-xl' />}</p>
+
+            {!passwordMatch ? <span className="text-red-500 text-sm">Passwords do not match</span> : ''}
+
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <input className="btn bg-base-300 rounded-none" type="submit" value='signup' disabled={passwordMatch} /> {/* Disable the button if passwords don't disabled={!passwordMatch} match */}
+          </div>
+          <Link to='/login'><p className='text-sm text-blue-700 underline'>Already have an account? Login.</p></Link>
           <div className="divider">OR</div>
-          </form>
-          <div className='text-center mb-5'>
-            <button className="btn bg-base-300 rounded-none w-5/6">Sign In with <FcGoogle className='text-2xl'/></button>
-          </div>
-          </div>
+        </form>
+
+
+        <div className='text-center mb-5'>
+          <button className="btn bg-base-300 rounded-none w-5/6">Sign In with <FcGoogle className='text-2xl' /></button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Register;
