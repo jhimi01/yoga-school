47b084cda966051dc04382a92470a6ec379634a7
jhimi01 <talukderjhimi@gmail.com>
2025-01-07 21:13:43 +0600

    requie

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: [
     "./index.html",
@@ -7,9 +9,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
   daisyui: {
     themes: ["light", "dark", "cupcake"],
   },
-}
-
+};
